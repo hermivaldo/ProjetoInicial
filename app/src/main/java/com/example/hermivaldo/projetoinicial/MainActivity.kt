b3497f999b0c22de3ffb3f930096afe2340d300c
hermivaldo <hermivaldo.braga@gmail.com>
2018-04-19 16:44:50 -0300

    Inclusão do primeiro fragmento da aplicação.

--- app/src/main/java/com/example/hermivaldo/projetoinicial/MainActivity.kt
+++ app/src/main/java/com/example/hermivaldo/projetoinicial/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hermivaldo.projetoinicial
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+    }
+
+    fun realizarLogin(view: View){
+        var newPage = Intent(this, Painel::class.java)
+        startActivity(newPage)
     }
 }
--- app/src/main/java/com/example/hermivaldo/projetoinicial/Painel.kt
+++ app/src/main/java/com/example/hermivaldo/projetoinicial/Painel.kt
@@ -2,27 +2,36 @@ package com.example.hermivaldo.projetoinicial
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.example.hermivaldo.projetoinicial.fragments.ListFragment
 import kotlinx.android.synthetic.main.activity_painel.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class Painel : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                changeFragment(ListFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                //message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                //message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
+    }
+
+    fun changeFragment(fragment: Fragment){
+        var transition = supportFragmentManager.beginTransaction()
+        transition.replace(R.id.mainFragm, fragment)
+        transition.commit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,5 +39,7 @@ class Painel : AppCompatActivity() {
         setContentView(R.layout.activity_painel)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        changeFragment(ListFragment())
+        actionBar?.hide()
     }
 }
