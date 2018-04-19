c76ef89f2d97d4a8e2639db95186a165e6bd1bcc
Hermivaldo Braga Oliveira Junior <hermivaldo@brq.com>
2018-04-19 15:55:10 -0300

    Finaliação da tela de login da aplicação

---
+++ app/src/main/java/com/example/hermivaldo/projetoinicial/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hermivaldo.projetoinicial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+    }
+}
